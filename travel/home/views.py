22fc0550f8f3ffc1a995dcd2a043b8f895f52715
shravan64bit <shravanms321@gmail.com>
2025-10-15 23:02:33 +0530

    django-woking

---
+++ travel/home/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request,'index.html')
+def about(request):
+    return render(request,'about.html')
+# Create your views here.
+def places(request):
+    return HttpResponse("Places")
+def booking(request):
+    return HttpResponse("Booking")
